3510eb9320f20a05b9ef7d4af60e3d4854713acf
ArtyomKaltovich <KaltovichArtyom@gmail.com>
2020-02-24 13:59:20 +0300

    hw1 task2

---
+++ hw1/lab_notes.md
@@ -0,0 +1,6 @@
+* 24.02.2020
+    - creating python script with Biopython task2/main.py
+    - creating png and svg plots (tree.png tree.svg)
+    - converting data newick in data.txt phyloxml in data.phyloxml
+    - creating requirements file requirements.txt
+    
---
+++ hw1/task2/main.py
@@ -0,0 +1,30 @@
+import requests
+import matplotlib.pyplot as plt
+from Bio import Phylo
+
+DATA_URL = r"https://www.jasondavies.com/tree-of-life/life.txt"
+INPUT_FILE_NAME = "data.txt"
+OUT_FILE_NAME = "data.phyloxml"
+
+
+def save_data_to_file(data_url, file_name):
+    data = requests.get(data_url)
+    data = data.text
+    with open(file_name, "w") as file:
+        file.write(data)
+
+
+if __name__ == '__main__':
+    # save data as file, coz Phylo doesn't accept strings
+    # tempfile is also doesn't work and always empty
+    save_data_to_file(DATA_URL, INPUT_FILE_NAME)
+    data = Phylo.parse(INPUT_FILE_NAME, "newick")
+    fig = plt.figure(figsize=(25, 30))
+    axes = fig.add_subplot(1, 1, 1)
+    for tree in data:
+        tree.ladderize()
+        Phylo.draw_ascii(tree)
+        Phylo.draw(tree, do_show=False, axes=axes)
+        fig.savefig("tree.svg")
+        fig.savefig("tree.png")
+    Phylo.convert(INPUT_FILE_NAME, "newick", OUT_FILE_NAME, "phyloxml")
